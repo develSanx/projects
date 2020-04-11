86ba09fff221115c57b9c88dc8576fc0b676b1ed
Sanx Dev <develSanx@Sanx-Workstation>
2020-04-11 11:09:39 +0530

    initial

---
+++ Learnish/precache-manifest.a9d3cbb0d68eea7367095421dea9965b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a898ef575d5ab883616adca4ed58db09",
+    "url": "/projects/Learnish/index.html"
+  },
+  {
+    "revision": "1a546677d9ab2833101d",
+    "url": "/projects/Learnish/static/css/2.9f4a86d1.chunk.css"
+  },
+  {
+    "revision": "f660468705a83a852b62",
+    "url": "/projects/Learnish/static/css/main.0fb80e35.chunk.css"
+  },
+  {
+    "revision": "1a546677d9ab2833101d",
+    "url": "/projects/Learnish/static/js/2.12249075.chunk.js"
+  },
+  {
+    "revision": "f660468705a83a852b62",
+    "url": "/projects/Learnish/static/js/main.f99b5163.chunk.js"
+  },
+  {
+    "revision": "fa3acfcc3c741cfac807",
+    "url": "/projects/Learnish/static/js/runtime-main.068a38fe.js"
+  },
+  {
+    "revision": "86183d9670d47b1099cc93e48439c686",
+    "url": "/projects/Learnish/static/media/P1.86183d96.jpg"
+  },
+  {
+    "revision": "de0acf3329d089e1a99a770b7ea2ad1a",
+    "url": "/projects/Learnish/static/media/P2.de0acf33.jpg"
+  },
+  {
+    "revision": "fdae38cef91d9e033ef38d768b1bd602",
+    "url": "/projects/Learnish/static/media/P3.fdae38ce.jpg"
+  },
+  {
+    "revision": "5c557e29b2b9abfe9ac5f43e260a1d45",
+    "url": "/projects/Learnish/static/media/P4.5c557e29.jpg"
+  },
+  {
+    "revision": "18debe05323bacea2b9025af14db799c",
+    "url": "/projects/Learnish/static/media/abtBG.18debe05.jpg"
+  },
+  {
+    "revision": "7961feb4ae175893998edf99da5566e9",
+    "url": "/projects/Learnish/static/media/hero_Bg.7961feb4.jpg"
+  },
+  {
+    "revision": "adaae096258715eef884663c55a30b28",
+    "url": "/projects/Learnish/static/media/stud.adaae096.png"
+  }
+]);
